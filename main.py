c02c22810e9728c7ba8abb300a464acb72d24424
unknown <gabworks51@gmail.com>
2018-09-11 01:26:10 +0300

    feature (description) If the data is not valid, we don’t have to worry about it, the error message will be sent to the user. But if we pass valid data, everything works fine. [delivers #160404691]

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful.reqparse import RequestParser
 
 app = Flask(__name__)
 api = Api(app, prefix="/api/v1")
 
 users = [
-    { "id": 1,"name": "Masnun", "email": "masnun@gmail.com"},
-    { "id": 2,"name": "Joseph", "email": "joseph@gmail.com"}
-    { "id": 1,"name": "Gabriel", "email": "gabriel@gmail.com"}
-    { "id": 1,"name": "Gitonga", "email": "gitonga@gmail.com"}
-    { "id": 1,"name": "Melvin", "email": "melvin@gmail.com"}
+    {"email": "masnun@gmail.com", "name": "Masnun", "id": 1}
 ]
+
+customer_request_parser = RequestParser(bundle_errors=True)
+customer_request_parser.add_argument("name", type=str, required=True, help="Name has to be valid string")
+customer_request_parser.add_argument("email", required=True)
+customer_request_parser.add_argument("id", type=int, required=True, help="Please enter valid integer as ID")
 
 
 class CustomerCollection(Resource):
     def get(self):
-        return {"msg": "All Customers "}
+        return users
 
     def post(self):
-        return {"msg": "We will create new Customers here"}
+        args = subscriber_request_parser.parse_args()
+        users.append(args)
+        return {"msg": "Subscriber added", "subscriber_data": args}
 
 
 class Customer(Resource):
@@ -32,8 +36,8 @@ class Customer(Resource):
         return {"msg": "Delete user id {}".format(id)}
 
 
-api.add_resource(CustomerCollection, '/Customers')
-api.add_resource(Customer, '/Customers/<int:id>')
+api.add_resource(CustomerCollection, '/users')
+api.add_resource(Customer, '/users/<int:id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
