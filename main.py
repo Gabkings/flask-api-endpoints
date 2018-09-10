ee4999b24aa7309463fa1b1b7abb38351c6e3395
unknown <gabworks51@gmail.com>
2018-09-11 01:04:48 +0300

    feature (description) have a basic skeleton ready and we know how to test my API Added the url versioning for api data  access [delivers #160404371]

--- main.py
+++ main.py
@@ -2,15 +2,38 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, prefix="/api/v1")
+
+users = [
+    { "id": 1,"name": "Masnun", "email": "masnun@gmail.com"},
+    { "id": 2,"name": "Joseph", "email": "joseph@gmail.com"}
+    { "id": 1,"name": "Gabriel", "email": "gabriel@gmail.com"}
+    { "id": 1,"name": "Gitonga", "email": "gitonga@gmail.com"}
+    { "id": 1,"name": "Melvin", "email": "melvin@gmail.com"}
+]
 
 
-class HelloWorld(Resource):
+class CustomerCollection(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {"msg": "All Customers "}
+
+    def post(self):
+        return {"msg": "We will create new Customers here"}
 
 
-api.add_resource(HelloWorld, '/')
+class Customer(Resource):
+    def get(self, id):
+        return {"msg": "Details about user id {}".format(id)}
+
+    def put(self, id):
+        return {"msg": "Update user id {}".format(id)}
+
+    def delete(self, id):
+        return {"msg": "Delete user id {}".format(id)}
+
+
+api.add_resource(CustomerCollection, '/Customers')
+api.add_resource(Customer, '/Customers/<int:id>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
